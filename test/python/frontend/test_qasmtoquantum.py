2735e5f02aef08bfa5c4b8f1f7ecff15f9c65315
Lars Schütze <lars.schuetze@tu-dresden.de>
2026-02-27 10:57:24 +0100

    Fix python test cases

--- test/python/frontend/test_qasmtoquantum.py
+++ test/python/frontend/test_qasmtoquantum.py
@@ -1,9 +1,10 @@
 # RUN: %PYTHON %s | FileCheck %s
 
+
 from mlir_quantum.dialects import arith
 from mlir_quantum.dialects.builtin import Context, IndexType, InsertionPoint, Location
 
-from frontend.qasm.qasmtoquantum import Interval
+from frontend.qasm.qasmtoquantum import Interval, IntervalMap
 
 
 def run(f):
@@ -12,7 +13,7 @@ def run(f):
     return f
 
 
-def mock_value(v: int = 0, context: Context | None = None, loc: Location | None = None, ip: InsertionPoint | None = None):
+def mock_value(v: int = 0, *, context: Context | None = None, loc: Location | None = None, ip: InsertionPoint | None = None):
     return arith.ConstantOp(
         IndexType.get(context),
         v,
@@ -23,88 +24,156 @@ def mock_value(v: int = 0, context: Context | None = None, loc: Location | None 
 # CHECK-LABEL: TEST: test_interval_contains
 @run
 def test_interval_contains():
-    with Context() as ctx, Location.unknown() as loc:
+    with Context(), Location.unknown():
         interval = Interval(5, 10, mock_value())
 
-        assert interval.contains(5)  # Lower bound inclusive
-        assert interval.contains(9)  # Upper bound exclusive
-        assert not interval.contains(10)  # Upper bound exclusive
-        assert not interval.contains(4)  # Below lower bound
+        # Lower bound inclusive
+        assert interval.contains(5)
+        # Upper bound exclusive
+        assert interval.contains(9)
+        # Upper bound exclusive
+        assert not interval.contains(10)
+        # Below lower bound
+        assert not interval.contains(4)
+
 
 # CHECK-LABEL: TEST: test_interval_length
 @run
 def test_interval_length():
-    interval = Interval(5, 10, mock_value())
-    assert(len(interval) == 5)  # Length is end - start
+    with Context(), Location.unknown():
+        interval = Interval(5, 10, mock_value())
+        # Length is end - start
+        assert len(interval) == 5
 
+
+# CHECK-LABEL: TEST: test_interval_equality
 @run
 def test_interval_equality():
-    interval1 = Interval(5, 10, mock_value(1))
-    interval2 = Interval(5, 10, mock_value(2))  # Different value, same bounds
-    assert(interval1 == interval2)  # Equality based on start and end
+    with Context(), Location.unknown():
+        interval1 = Interval(5, 10, mock_value(1))
+        # Different value, same bounds
+        interval2 = Interval(5, 10, mock_value(2))
+        # Equality based on start and end
+        assert interval1 == interval2
 
 
-# class TestIntervalMap(unittest.TestCase):
-#     def setUp(self):
-#         self.map = IntervalMap()
+# CHECK-LABEL: TEST: test_add_interval
+@run
+def test_add_interval():
+    with Context(), Location.unknown():
+        map = IntervalMap()
+        mock = mock_value()
+        map.add(0, 5, mock)
+        assert len(map) == 1
+        assert map.get(0) == mock
+        assert map.get(4) == mock
+        # Outside the interval
+        assert map.get(5) is None
 
-#     def test_add_interval(self):
-#         self.map.add(0, 5, "value1")
-#         self.assertEqual(len(self.map), 1)
-#         self.assertEqual(self.map.get(0), "value1")
-#         self.assertEqual(self.map.get(4), "value1")
-#         self.assertIsNone(self.map.get(5))  # Outside the interval
 
-#     def test_add_overlapping_intervals(self):
-#         self.map.add(0, 5, "value1")
-#         with self.assertRaises(ValueError):
-#             self.map.add(4, 10, "value2")  # Overlaps with existing interval
-#         with self.assertRaises(ValueError):
-#             self.map.add(5, 10, "value3")  # Touching the end of the first interval
+# CHECK-LABEL: TEST: test_add_interval_overlap
+@run
+def test_add_interval_overlap():
+    with Context(), Location.unknown():
+        map = IntervalMap()
+        map.add(0, 5, mock_value())
+        try:
+            # Overlaps with existing interval
+            map.add(4, 10, mock_value())
+        except ValueError:
+            pass
+        else:
+            raise AssertionError("Expected ValueError due to overlapping interval")
 
-#     def test_remove_interval(self):
-#         self.map.add(0, 5, "value1")
-#         self.map.add(10, 15, "value2")
-#         self.assertEqual(self.map.remove(0, 5), "value1")
-#         self.assertEqual(len(self.map), 1)
-#         with self.assertRaises(KeyError):
-#             self.map.remove(0, 5)  # Interval already removed
 
-#     def test_get_interval(self):
-#         self.map.add(0, 5, "value1")
-#         self.map.add(10, 15, "value2")
-#         self.assertEqual(self.map.get(0), "value1")
-#         self.assertEqual(self.map.get(4), "value1")
-#         self.assertIsNone(self.map.get(5))  # Outside any interval
-#         self.assertEqual(self.map.get(10), "value2")
+# CHECK-LABEL: TEST: test_add_interval_overlap_upper_lower
+@run
+def test_add_interval_overlap_upper_lower():
+    with Context(), Location.unknown():
+        map = IntervalMap()
+        map.add(0, 5, mock_value())
+        try:
+            # Touching the end of the first interval
+            map.add(5, 10, mock_value())
+        except Exception as e:
+            raise AssertionError("Unexpected exception") from e
 
-#     def test_interval_containing(self):
-#         self.map.add(0, 5, "value1")
-#         self.map.add(10, 15, "value2")
-#         interval = self.map.interval_containing(3)
-#         self.assertEqual(interval.start, 0)
-#         self.assertEqual(interval.end, 5)
-#         self.assertEqual(interval.value, "value1")
-#         with self.assertRaises(KeyError):
-#             self.map.interval_containing(6)  # No interval contains this key
 
-#     def test_replace_interval(self):
-#         self.map.add(0, 5, "value1")
-#         old_interval = self.map.interval_containing(3)
-#         new_intervals = [
-#             Interval(0, 2, "new_value1"),
-#             Interval(3, 5, "new_value2"),
-#         ]
-#         self.map.replace_interval(old_interval, new_intervals)
-#         self.assertEqual(len(self.map), 2)
-#         self.assertEqual(self.map.get(1), "new_value1")
-#         self.assertEqual(self.map.get(4), "new_value2")
-#         self.assertIsNone(self.map.get(5))  # Outside any interval
+# CHECK-LABEL: TEST: test_remove_interval
+@run
+def test_remove_interval():
+    with Context(), Location.unknown():
+        map = IntervalMap()
+        mock1 = mock_value()
+        map.add(0, 5, mock1)
+        map.add(10, 15, mock_value())
+        assert len(map) == 2
+        assert map.remove(0, 5) == mock1
+        assert len(map) == 1
+        try:
+            # Interval already removed
+            map.remove(0, 5)
+        except KeyError:
+            pass
+        else:
+            raise AssertionError("Expected KeyError due to no-existing interval")
 
-#     def test_repr(self):
-#         self.map.add(0, 5, "value1")
-#         self.map.add(10, 15, "value2")
-#         self.assertEqual(
-#             repr(self.map),
-#             "IntervalMap([Interval(start=0, end=5, value='value1'), Interval(start=10, end=15, value='value2')])"
-#         )
+
+# CHECK-LABEL: TEST: test_get_interval
+@run
+def test_get_interval():
+    with Context(), Location.unknown():
+        map = IntervalMap()
+        mock1 = mock_value()
+        mock2 = mock_value()
+        map.add(0, 5, mock1)
+        map.add(10, 15, mock2)
+        assert map.get(0) == mock1
+        assert map.get(4) == mock1
+        # Outside any interval
+        assert map.get(5) is None
+        assert map.get(10) == mock2
+
+
+# CHECK-LABEL: TEST: test_interval_containing
+@run
+def test_interval_containing():
+    with Context(), Location.unknown():
+        map = IntervalMap()
+        mock1 = mock_value()
+        mock2 = mock_value()
+        map.add(0, 5, mock1)
+        map.add(10, 15, mock2)
+        interval = map.interval_containing(3)
+        assert interval.start == 0
+        assert interval.end == 5
+        assert interval.value == mock1
+        try:
+            # No interval contains this key
+            map.interval_containing(6)
+        except KeyError:
+            pass
+        else:
+            raise AssertionError("Expected KeyError due to non-existing interval")
+
+
+# CHECK-LABEL: TEST: test_replace_interval
+@run
+def test_replace_interval():
+    with Context(), Location.unknown():
+        map = IntervalMap()
+        mock1 = mock_value()
+        mock2 = mock_value()
+        mock3 = mock_value()
+        map.add(0, 5, mock1)
+        old_interval = map.interval_containing(3)
+        new_intervals = [
+            Interval(0, 2, mock2),
+            Interval(2, 5, mock3),
+        ]
+        map.replace_interval(old_interval, new_intervals)
+        assert len(map) == 2
+        assert map.get(1) == mock2
+        assert map.get(4) == mock3
+        # Outside any interval
+        assert map.get(5) is None
